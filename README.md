897c2d81b73506e982fe98592ebbe14e1aff0725
Maciej Preus <mpreus@onet.eu>
2020-01-16 22:01:50 +0100

    Add weather data fetched and README file

---
+++ README.md
@@ -0,0 +1,2 @@
+Current weather situation and forecast for different location.
+API in use as well as vanila JavaScript (ES6).
--- script/script.js
+++ script/script.js
@@ -1,7 +1,43 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-	console.log("ŁĄCZĘ!");
-
 	
+/* variables for keeping current position - longitude and latitude */
+	let long;
+	let lat;
+
+	let temperatureDescription = document.querySelector(".temperature-description");
+	let temperatureDegree = document.querySelector(".temperature-degree");
+	let locationTimezone = document.querySelector(".location-timezone");
+
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+		/* to avoid CORS policy consequences, we use 'cors-anywhere' */
+			const proxy = "https://cors-anywhere.herokuapp.com/";
+			const api = `${proxy}https://api.darksky.net/forecast/e25dfc79138af46ff1990edafb63c5fe/${lat},${long}`;
+			fetch(api)
+				.then(response => {
+					return response.json();
+				})
+				.then(data => {
+					console.log(data); /* allows us to see in the console what's available */
+					/* destructuring allow us to use selected elements */
+					const {temperature, summary} = data.currently;
+					/* DOM elements build with API */
+					temperatureDegree.textContent = temperature;
+					temperatureDescription.textContent = summary;
+					locationTimezone.textContent = data.timezone;
+				})
+		});
+	}
 }
+
+
+
+
+
+
+
